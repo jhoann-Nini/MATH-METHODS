c1f41bd0b72b219f4d264b4014568814678a0426
ninijh <nini08ca@gmail.com>
2026-06-10 16:29:33 -0500

    agrega modelo de iteracion

---
+++ src/js/models/Iteration.js
@@ -0,0 +1,26 @@
+export default class Iteration {
+
+
+constructor(
+n,
+a,
+b,
+c,
+fc,
+errAbs,
+errRel
+){
+
+this.n=n;
+this.a=a;
+this.b=b;
+this.c=c;
+this.fc=fc;
+this.errAbs=errAbs;
+this.errRel=errRel;
+
+
+}
+
+
+}
